df56e07135a5f2d2f5a4b540d078de90eb8a4e89
Ido Bar <ido.idobar@gmail.com>
2021-01-12 02:21:21 +1000

    Fix java modules accessibility

--- modules/nf-commons/src/main/nextflow/Const.groovy
+++ modules/nf-commons/src/main/nextflow/Const.groovy
@@ -58,12 +58,12 @@ class Const {
     /**
      * The app build time as linux/unix timestamp
      */
-    static public final long APP_TIMESTAMP = 1609793259654
+    static public final long APP_TIMESTAMP = 1610377815710
 
     /**
      * The app build number
      */
-    static public final int APP_BUILDNUM = 5477
+    static public final int APP_BUILDNUM = 5478
 
 
     /**
